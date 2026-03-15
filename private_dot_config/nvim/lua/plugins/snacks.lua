87ff4aad678502e3810231cc0ab7a14bf5885a00
Pavel Dydyshka <pav2001pav@gmail.com>
2026-03-16 00:21:07 +0100

    nvim: remap <leader>sR to always continue grep

---
+++ private_dot_config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,13 @@
+return {
+  "folke/snacks.nvim",
+  keys = {
+    {
+      "<leader>sR",
+      function()
+        require("snacks")
+        Snacks.picker.resume({ source = "grep" })
+      end,
+      desc = "Resume grep",
+    },
+  },
+}
