e6daf6720aa74d439c4ceb5ad37a1704af165fad
Pavel Dydyshka <paul.dydyshko@gmail.com>
2026-03-25 19:24:24 +0100

    feat(nvim): add diffview plugin

---
+++ private_dot_config/nvim/lua/plugins/diffview.lua
@@ -0,0 +1,15 @@
+return {
+  "sindrets/diffview.nvim",
+  keys = {
+    {
+      "<leader>gm",
+      "<cmd>DiffviewOpen<cr>",
+      desc = "Open DiffView merge tool",
+    },
+    {
+      "<leader>gM",
+      "<cmd>DiffviewClose<cr>",
+      desc = "Close DiffView merge tool",
+    },
+  },
+}
