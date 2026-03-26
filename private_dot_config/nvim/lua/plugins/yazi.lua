7ac9acb535cbffd3b8cdb05c153cc34c0ebdbbcd
Pavel Dydyshka <paul.dydyshko@gmail.com>
2026-03-26 12:52:20 +0100

    refactor(nvim): rebind yazi plugin

--- private_dot_config/nvim/lua/plugins/yazi.lua
+++ private_dot_config/nvim/lua/plugins/yazi.lua
@@ -5,21 +5,19 @@ return {
   },
   event = "VeryLazy",
   keys = {
-    -- 👇 in this section, choose your own keymappings!
     {
-      "<leader>-",
+      "<leader>yc",
       mode = { "n", "v" },
       "<cmd>Yazi<cr>",
       desc = "Open yazi at the current file",
     },
     {
-      -- Open in the current working directory
-      "<leader>cw",
+      "<leader>yw",
       "<cmd>Yazi cwd<cr>",
       desc = "Open the file manager in nvim's working directory",
     },
     {
-      "<c-up>",
+      "<leader>yt",
       "<cmd>Yazi toggle<cr>",
       desc = "Resume the last yazi session",
     },
