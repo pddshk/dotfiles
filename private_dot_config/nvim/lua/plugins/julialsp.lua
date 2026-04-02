e2bae5a8fbf5341edbedadab48c09d3e3dcad953
Pavel Dydyshka <paul.dydyshko@gmail.com>
2026-04-02 02:51:43 +0200

    feat(nvim)!: remove julia lsp overrides from nvim-lspconfig config

--- private_dot_config/nvim/lua/plugins/julialsp.lua
+++ private_dot_config/nvim/lua/plugins/julialsp.lua
@@ -1,12 +1,10 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      -- Remove julials from the servers table so Mason doesn't try to manage it
-      servers = {},
-    },
-    init = function()
-      vim.lsp.enable("julials")
-    end,
-  },
+  -- "neovim/nvim-lspconfig",
+  -- opts = {
+  --   -- Remove julials from the servers table so Mason doesn't try to manage it
+  --   servers = {},
+  -- },
+  -- init = function()
+  --   vim.lsp.enable("julials")
+  -- end,
 }
