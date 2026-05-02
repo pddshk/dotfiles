a404322e1866a12ebb731862ba8de472f2ffb4e1
Pavel Dydyshka <paul.dydyshko@gmail.com>
2026-05-02 23:03:43 +0200

    feat(nvim): add treesitter plugin

---
+++ private_dot_config/nvim/lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,12 @@
+return {
+  'nvim-treesitter/nvim-treesitter',
+  build = ':TSUpdate',
+  event = { 'BufReadPost', 'BufNewFile' },
+  config = function()
+    require('nvim-treesitter.configs').setup({
+      ensure_installed = { 'lua', 'python', 'markdown' },
+      highlight = { enable = true },
+      indent = { enable = true },
+    })
+  end
+}
