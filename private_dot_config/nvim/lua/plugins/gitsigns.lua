2c854625aa5efba47278cee1c698a9351ee40cc7
Pavel Dydyshka <paul.dydyshko@gmail.com>
2026-05-03 20:11:49 +0200

    feat(nvim): tweak gitsigns signs

--- private_dot_config/nvim/lua/plugins/gitsigns.lua
+++ private_dot_config/nvim/lua/plugins/gitsigns.lua
@@ -2,6 +2,21 @@ return {
     "lewis6991/gitsigns.nvim",
     event = { 'BufReadPost', 'BufNewFile' },
     opts = {
+      signs = {
+        add = { text = "▎" },
+        change = { text = "▎" },
+        delete = { text = "🞂" },
+        topdelete = { text = "🞂" },
+        changedelete = { text = "▎" },
+        untracked = { text = "▎" },
+      },
+      signs_staged = {
+        add = { text = "▎" },
+        change = { text = "▎" },
+        delete = { text = "🞂" },
+        topdelete = { text = "🞂" },
+        changedelete = { text = "▎" },
+      },
       on_attach = function(buffer)
         local gitsigns = package.loaded.gitsigns
 
