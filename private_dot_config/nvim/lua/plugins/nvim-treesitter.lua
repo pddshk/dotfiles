8e70576203573b2726246f1cd32c1369f41094bf
Pavel Dydyshka <pav2001pav@gmail.com>
2026-05-04 04:11:28 +0200

    fix(nvim): disable lazy-loading of treesitter

--- private_dot_config/nvim/lua/plugins/nvim-treesitter.lua
+++ private_dot_config/nvim/lua/plugins/nvim-treesitter.lua
@@ -2,7 +2,7 @@ return {
   'nvim-treesitter/nvim-treesitter',
   branch = 'main',
   build = ':TSUpdate',
-  event = { 'BufReadPost', 'BufNewFile' },
+  lazy = false,
   config = function()
     require('nvim-treesitter.config').setup({
       highlight = { enable = true },
