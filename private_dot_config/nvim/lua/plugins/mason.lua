41eaf667145513e4fb9e81906f9ce6a5f55e8d6a
Pavel Dydyshka <pav2001pav@gmail.com>
2026-05-04 04:32:29 +0200

    feat(nvim): add mason-lspconfig

--- private_dot_config/nvim/lua/plugins/mason.lua
+++ private_dot_config/nvim/lua/plugins/mason.lua
@@ -1,6 +1,18 @@
 return {
-  "mason-org/mason.nvim",
-  config = function()
-    require('mason').setup()
-  end
+  {
+    "mason-org/mason.nvim",
+    config = function()
+      require('mason').setup()
+    end
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    dependencies = {
+      "mason-org/mason.nvim",
+      "neovim/nvim-lspconfig",
+    },
+    opts = {
+      ensure_installed = { "lua_ls" }
+    },
+  }
 }
