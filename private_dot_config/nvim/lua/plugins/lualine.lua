10983b097c5879df784affda0d6b093554f14907
Pavel Dydyshka <paul.dydyshko@gmail.com>
2026-05-15 23:03:36 +0200

    feat(nvim): reformat lualine

--- private_dot_config/nvim/lua/plugins/lualine.lua
+++ private_dot_config/nvim/lua/plugins/lualine.lua
@@ -1,35 +1,28 @@
 return {
   "nvim-lualine/lualine.nvim",
-  event = "VeryLazy",
   opts = {
-    options = {
-      theme = "tokyonight",
-      globalstatus = true,
-      component_separators = { left = "", right = "" },
-      section_separators = { left = "", right = "" },
-    },
     sections = {
-      lualine_a = { "mode" },
-      lualine_b = { "branch" },
-      lualine_c = { { "filename", path = 1, symbols = { modified = "", readonly = "" } } },
-      lualine_x = { "searchcount" },
+      lualine_c = {},
+      lualine_x = {
+        "searchcount",
+        "encoding",
+        function()
+          local fmt = vim.bo.fileformat
+          if fmt == "unix" then
+            return "LF"
+          elseif fmt == "dos" then
+            return "CRLF"
+          else
+            return "CR"
+          end
+        end,
+      },
       lualine_y = { "filetype" },
       lualine_z = {
-        {
-          "location",
-          fmt = function()
-            return vim.fn.line(".") .. "/" .. vim.fn.line("$")
-          end,
-        },
+        function()
+          return vim.fn.line(".") .. "/" .. vim.fn.line("$") .. ":" .. vim.fn.col(".")
+        end,
       },
-    },
-    inactive_sections = {
-      lualine_a = {},
-      lualine_b = {},
-      lualine_c = { { "filename", path = 1 } },
-      lualine_x = {},
-      lualine_y = {},
-      lualine_z = {},
     },
   },
 }
