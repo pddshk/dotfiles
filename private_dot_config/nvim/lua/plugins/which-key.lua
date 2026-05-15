eef53d90c4e815b23c6413e1695a2aa26d4cb711
Pavel Dydyshka <paul.dydyshko@gmail.com>
2026-05-15 12:14:56 +0200

    feat(nvim): add more which-key groups

--- private_dot_config/nvim/lua/plugins/which-key.lua
+++ private_dot_config/nvim/lua/plugins/which-key.lua
@@ -5,7 +5,27 @@ return {
     spec = {
       {
         mode = { "n", "x" },
-        { "<leader>y", group = "yazi" },
+        { "<leader>y", group = "Yazi" },
+      },
+      {
+        mode = { "n", "x" },
+        { "<leader>b", group = "Buffers"}
+      },
+      {
+        mode = { "n", "x" },
+        { "<leader>g", group = "git"}
+      },
+      {
+        mode = { "n", "x" },
+        { "<leader>gh", group = "hunks"}
+      },
+      {
+        mode = { "n", "x" },
+        { "<leader>u", group = "Tree-sitter"}
+      },
+      {
+        mode = { "n", "x" },
+        { "<leader>c", group = "Code"}
       },
     },
   },
