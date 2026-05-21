86fe94ed0b1bdcf04fb8db06ac44e808d53e6739
Pavel Dydyshka <paul.dydyshko@gmail.com>
2026-05-21 20:20:18 +0200

    feat(nvim): add log highlighter

---
+++ private_dot_config/nvim/lua/plugins/log-highlight.lua
@@ -0,0 +1,12 @@
+return {
+  'fei6409/log-highlight.nvim',
+  opts = {
+    keyword = {
+      info = 'INFO',
+      error = {'ERROR', 'CRITICAL', 'HACK'},
+      warning = 'WARNING',
+      debug = 'DEBUG',
+      pass = 'TRACE',
+    }
+  }
+}
