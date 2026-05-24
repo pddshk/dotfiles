112147eb4323cbb4680a740877c65614438bf1df
Pavel Dydyshka <pav2001pav@gmail.com>
2026-05-24 16:37:01 +0200

    feat(nvim): add mini.animate

--- private_dot_config/nvim/lua/plugins/mini.lua
+++ private_dot_config/nvim/lua/plugins/mini.lua
@@ -2,4 +2,5 @@ return {
   { 'nvim-mini/mini.pairs',     opts = {} },
   { 'nvim-mini/mini.splitjoin', opts = {} },
   { 'nvim-mini/mini.surround',  opts = {} },
+  { 'nvim-mini/mini.animate',   opts = {} },
 }
