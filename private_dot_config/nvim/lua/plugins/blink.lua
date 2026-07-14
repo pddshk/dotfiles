6435e2dd11fde1b9eaf6bd7208c9460a7aff3ff6
Pavel Dydyshka <paul.dydyshko@gmail.com>
2026-07-14 13:17:48 +0200

    feat(nvim): make cmd autocompletion sane

--- private_dot_config/nvim/lua/plugins/blink.lua
+++ private_dot_config/nvim/lua/plugins/blink.lua
@@ -6,5 +6,13 @@ return {
       ["<S-Tab>"] = { "select_prev", "fallback" },
       ["<CR>"] = { "accept", "fallback" },
     },
+    cmdline = {
+      keymap = {
+        ["<Tab>"] = { "select_next", "fallback" },
+        ["<S-Tab>"] = { "select_prev", "fallback" },
+        ["<CR>"] = { "accept", "fallback" },
+      },
+      completion = { menu = { auto_show = true } },
+    },
   },
 }
